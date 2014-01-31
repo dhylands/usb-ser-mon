71daea9ea57b55f70ad381653b8da7c8c71de433
Dave Hylands <dhylands@gmail.com>
2014-01-31 14:43:32 -0800

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,69 @@
+usb-ser-mon.py
+==============
+
+A serial monitor for USB Serial devices.
+
+usb-ser-mon.py will automatically detect your connected USB Serial device and
+print the output from it.
+
+This is similar in functionality to the Arduino serial monitor, except that
+usb-ser-mon.py deals with the device disconnects automtically, and will wait
+for your device to reconnect.
+
+If you have more than one USB device connected, you can use the -s
+option to specify the serial number of the device you wish to connect to,
+or use the -n command to specify the device vendor.
+
+Currently, this program only works under linux.
+
+It was tested with the following devices:
+  - Teensy 3.1
+  - STM32F4DISCOVERY board
+  - Prolific USB to Serial adapter
+
+
+Use -l to list all of the connected devices.
+```
+./usb-ser-mon.py -l
+````
+
+will show you the currently connected devices, for example:
+```
+USB Serial Device with vendor 'Teensyduino' serial '21973' found @/dev/ttyACM1
+USB Serial Device with vendor 'Prolific_Technology_Inc.' found @/dev/ttyUSB0
+USB Serial Device with vendor 'STMicroelectronics' serial '00000000050C' found @/dev/ttyACM0
+```
+
+If you want to connect with the STM device (an STM32FDISCOVERY board in this situation), then you might do:
+```
+./usb-ser-mon.py -n Teensy
+```
+
+and then see:
+```
+USB Serial device with vendor 'Teensyduino' serial '21973' connected @/dev/ttyACM1
+
+>>>
+```
+
+In the previous example the Teensy was already connected. If I unplug and replug the Teensy device then I'd see:
+```
+USB Serial device @ /dev/ttyACM1  disconnected.
+
+Waiting for USB Serial Device with vendor 'Teensy' ...
+USB Serial device with vendor 'Teensyduino' serial '21973' connected @/dev/ttyACM1
+Done executing '/src/main.py'
+Micro Python for Teensy 3.1
+Type "help()" for more information.
+>>>
+```
+
+You only need to use as many characters as are required to uniquely identify a
+device, so I could use ```./usb-ser-mon.py -n STM``` to connect to the
+Discovery board.
+
+Use Control-C to exit from usb-ser-mon.py.
+
+
+
+
---
+++ usb-ser-mon.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python -u
+
+"""Program which auto-connects to USB serial devices.
+
+This program waits for the device to be connected and when the device is
+disconnected, then it will go back to waiting for a device to once again
+be connected.
+
+"""
+
+import select
+import pyudev
+import serial
+import sys
+import tty
+import termios
+import traceback
+import syslog
+import argparse
+
+
+def is_usb_serial(device, serial_num=None, vendor=None):
+    """Checks device to see if its a USB Serial device.
+
+    The caller already filters on the subsystem being 'tty'.
+
+    If serial_num or vendor is provided, then it will further check to
+    see if the serial number and vendor of the device also matches.
+    """
+    if 'ID_VENDOR' not in device:
+        return False
+    if not vendor is None:
+        if not device['ID_VENDOR'].startswith(vendor):
+            return False
+    if not serial_num is None:
+        if device['ID_SERIAL_SHORT'] != serial_num:
+            return False
+    return True
+
+
+def extra_info(device):
+    extra_items = []
+    if 'ID_VENDOR' in device:
+        extra_items.append("vendor '%s'" % device['ID_VENDOR'])
+    if 'ID_SERIAL_SHORT' in device:
+        extra_items.append("serial '%s'" % device['ID_SERIAL_SHORT'])
+    if extra_items:
+        return ' with ' + ' '.join(extra_items)
+    return ''
+
+
+def usb_serial_mon(monitor, device):
+    """Monitors the serial port from a given USB serial device.
+
+    This function open the USB serial port associated with device, and
+    will read characters from it and send to stdout. It will also read
+    characters from stdin and send them to the device.
+
+    This function returns when the deivce disconnects (or is
+    disconnected).
+
+    """
+    port_name = device.device_node
+    print 'USB Serial device%s connected @%s\r' % (
+        extra_info(device), port_name)
+    print 'Use Control-C to exit.\r'
+    epoll = select.epoll()
+    epoll.register(monitor.fileno(), select.POLLIN)
+
+    try:
+        serial_port = serial.Serial(port=port_name,
+                                    timeout=0.001,
+                                    bytesize=serial.EIGHTBITS,
+                                    parity=serial.PARITY_NONE,
+                                    stopbits=serial.STOPBITS_ONE,
+                                    xonxoff=False,
+                                    rtscts=False,
+                                    dsrdtr=False)
+    except serial.serialutil.SerialException:
+        print "Unable to open port '%s'\r" % port_name
+        return
+
+    serial_fd = serial_port.fileno()
+    tty.setraw(serial_fd)
+    new_settings = termios.tcgetattr(serial_fd)
+    new_settings[6][termios.VTIME] = 0
+    new_settings[6][termios.VMIN] = 1
+    termios.tcsetattr(serial_fd, termios.TCSANOW, new_settings)
+
+    epoll.register(serial_port.fileno(), select.POLLIN)
+    epoll.register(sys.stdin.fileno(), select.POLLIN)
+
+    while True:
+        events = epoll.poll()
+        for fileno, _ in events:
+            if fileno == monitor.fileno():
+                dev = monitor.poll()
+                if (dev.device_node != port_name or
+                        dev.action != 'remove'):
+                    continue
+                print 'USB Serial device @', port_name, ' disconnected.\r'
+                print '\r'
+                serial_port.close()
+                return
+            if fileno == serial_port.fileno():
+                try:
+                    data = serial_port.read(256)
+                except serial.serialutil.SerialException:
+                    print 'USB Serial device @', port_name, ' disconnected.\r'
+                    print '\r'
+                    serial_port.close()
+                    return
+                #for x in data:
+                #    print "Serial.Read '%c' 0x%02x\r" % (x, ord(x))
+                sys.stdout.write(data)
+                sys.stdout.flush()
+            if fileno == sys.stdin.fileno():
+                data = sys.stdin.read(1)
+                #for x in data:
+                #    print "stdin.Read '%c' 0x%02x\r" % (x, ord(x))
+                if data[0] == chr(3):
+                    # Intercept Control-C and tell our program to exit
+                    raise KeyboardInterrupt
+                if data[0] == '\n':
+                    serial_port.write('\r')
+                else:
+                    serial_port.write(data)
+
+def main():
+    """The main program."""
+
+    parser = argparse.ArgumentParser(
+        prog="usb-ser-mon.py",
+        usage="%(prog)s [options] [command]",
+        description="Monitor serial output from USB Serial devices",
+        epilog="Press Control-C to quit"
+    )
+    parser.add_argument(
+        "-l", "--list",
+        dest="list",
+        action="store_true",
+        help="List USB Serial devices currently connected"
+    )
+    parser.add_argument(
+        "-s", "--serial",
+        dest="serial",
+        help="Connect to USB Serial device with a given serial number"
+    )
+    parser.add_argument(
+        "-n", "--vendor",
+        dest="vendor",
+        help="Connect to USB Serial device with a given vendor"
+    )
+    parser.add_argument(
+        "-v", "--verbose",
+        dest="verbose",
+        action="store_true",
+        help="Turn on verbose messages",
+        default=False
+    )
+    args = parser.parse_args(sys.argv[1:])
+
+    if args.verbose:
+        print 'pyudev version =', pyudev.__version__
+
+    context = pyudev.Context()
+    context.log_priority = syslog.LOG_NOTICE
+
+    if args.list:
+        detected = False
+        for device in context.list_devices(subsystem='tty'):
+            if is_usb_serial(device):
+                print 'USB Serial Device%s found @%s\r' % (
+                    extra_info(device), device.device_node)
+                detected = True
+        if not detected:
+            print 'No USB Serial devices detected.\r'
+        return
+
+    stdin_fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(stdin_fd)
+    try:
+        # Make some changes to stdin. We want to turn off canonical
+        # processing  (so that ^H gets sent to the device), turn off echo,
+        # and make it unbuffered.
+        tty.setraw(stdin_fd)
+        new_settings = termios.tcgetattr(stdin_fd)
+        new_settings[3] &= ~(termios.ICANON | termios.ECHO)
+        new_settings[6][termios.VTIME] = 0
+        new_settings[6][termios.VMIN] = 1
+        termios.tcsetattr(stdin_fd, termios.TCSANOW, new_settings)
+
+        monitor = pyudev.Monitor.from_netlink(context)
+        monitor.start()
+        monitor.filter_by('tty')
+
+        # Check to see if the USB Serial device is already present.
+        for device in context.list_devices(subsystem='tty'):
+            if is_usb_serial(device, serial_num=args.serial, vendor=args.vendor):
+                usb_serial_mon(monitor, device)
+
+        # Otherwise wait for the teensy device to connect
+        while True:
+            dev = {}
+            if args.serial:
+                dev['ID_SERIAL_SHORT'] = args.serial
+            if args.vendor:
+                dev['ID_VENDOR'] = args.vendor
+            print 'Waiting for USB Serial Device%s ...\r' % extra_info(dev)
+            epoll = select.epoll()
+            epoll.register(monitor.fileno(), select.POLLIN)
+            epoll.register(sys.stdin.fileno(), select.POLLIN)
+            while True:
+                events = epoll.poll()
+                for fileno, _ in events:
+                    if fileno == monitor.fileno():
+                        device = monitor.poll()
+                        if device.action != 'add':
+                            continue
+                        if is_usb_serial(device, serial_num=args.serial, vendor=args.vendor):
+                            usb_serial_mon(monitor, device)
+                            break
+                    if fileno == sys.stdin.fileno():
+                        data = sys.stdin.read(1)
+                        if data[0] == chr(3):
+                            # Intercept Control-C and tell our program to exit
+                            raise KeyboardInterrupt
+    except KeyboardInterrupt:
+        print '\r\n'
+    except Exception:
+        traceback.print_exc()
+    # Restore stdin back to its old settings
+    termios.tcsetattr(stdin_fd, termios.TCSANOW, old_settings)
+
+main()
