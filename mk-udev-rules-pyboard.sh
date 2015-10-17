1ec9b908360b88f0bc44b67e592906142c7b5140
Dave Hylands <dhylands@gmail.com>
2015-10-17 13:29:34 -0700

    Updated mk-udev-rules-pyboard.sh to reflect what I actually use.

--- mk-udev-rules-pyboard.sh
+++ mk-udev-rules-pyboard.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 sudo sh -c 'cat > /etc/udev/rules.d/49-pyboard.rules' <<EOF
-# f055:9800 MicroPython pyboard
-ATTRS{idVendor}=="f055", ATTRS{idProduct}=="9800", ENV{ID_MM_DEVICE_IGNORE}="1"
-ATTRS{idVendor}=="f055", ATTRS{idProduct}=="9800", ENV{MTP_NO_PROBE}="1"
-SUBSYSTEMS=="usb", ATTRS{idVendor}=="f055", ATTRS{idProduct}=="9800", MODE:="0666"
-KERNEL=="ttyACM*", ATTRS{idVendor}=="f055", ATTRS{idProduct}=="9800", MODE:="0666"
+# f055:9800, 9801, 9802 MicroPython pyboard
+ATTRS{idVendor}=="f055", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="f055", ENV{MTP_NO_PROBE}="1"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="f055", MODE:="0666"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="f055", MODE:="0666"
+
+# Add lines like the following to create custom named device nodes based on the
+# serial number.
+# KERNEL=="ttyACM*", ATTRS{idVendor}=="f055", ATTRS{serial}=="385435603432", SYMLINK+="pyboard", MODE:="0666"
 EOF
 
 sudo udevadm control --reload-rules
